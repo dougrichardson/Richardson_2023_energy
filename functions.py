17259aef629e97944172593933e378756bbf1543
Doug Richardson <54010293+dougrichardson@users.noreply.github.com>
2023-02-10 14:56:35 +1100

    Started surface data nb

---
+++ functions.py
@@ -0,0 +1,41 @@
+import xarray as xr
+
+# ============================================================================
+# Data wrangling
+# ============================================================================
+
+def get_REZ_boundary():
+    """
+    Returns a list of bounding coordinates for the REZ
+    regions (effectively eastern Australia).
+    """
+    return [133, 155, -10, -45]
+
+def open_era_data(root_path, variable, years, concat_dim='time',
+                 subset_region=None, lat_name='latitude',
+                 lon_name='longitude'):
+    """
+    Open ERA5 data from NCI.
+    
+    root_path: path to era5 data
+    variable: short name of variable used in path
+    years: range(first_year, last_year)
+    concat_dim: dimension name to concat over
+    subset_region: None, or select subregion using coordinates in a
+                    list like:[lon1, lon2, lat1, lat2]
+    lat_name: latitude dimension name
+    lonname: longitude dimension name
+    """
+    ds_list = []
+    for year in years:
+        fp = root_path+variable+'/'+str(year)+'/*.nc'
+        ds = xr.open_mfdataset(fp)
+        
+        if isinstance(subset_region, list):
+            ds = ds.sel({
+                lon_name: slice(subset_region[0], subset_region[1]),
+                lat_name: slice(subset_region[2], subset_region[3])
+            })
+            
+        ds_list.append(ds)
+    return xr.concat(ds_list, dim=concat_dim)
