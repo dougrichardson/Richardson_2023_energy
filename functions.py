2d870c881aa02e8ba66f296b8ed471338aa7fb70
Doug Richardson <54010293+dougrichardson@users.noreply.github.com>
2023-02-22 15:11:33 +1100

    Moved some functions to functions.py

--- functions.py
+++ functions.py
@@ -11,27 +11,7 @@ import cartopy
 import cartopy.crs as ccrs
 
 # ============================================================================
-# Misc
-# ============================================================================
-
-def round_decimals_up(number:float, decimals:int=2):
-    """
-    Returns a value rounded up to a specific number of decimal places.
-    
-    From: https://kodify.net/python/math/round-decimals/
-    """
-    if not isinstance(decimals, int):
-        raise TypeError("decimal places must be an integer")
-    elif decimals < 0:
-        raise ValueError("decimal places has to be 0 or more")
-    elif decimals == 0:
-        return math.ceil(number)
-
-    factor = 10 ** decimals
-    return math.ceil(number * factor) / factor
-
-# ============================================================================
-# Data wrangling
+# Masks
 # ============================================================================
 
 def get_REZ_boundary():
@@ -40,6 +20,21 @@ def get_REZ_boundary():
     regions (effectively eastern Australia).
     """
     return [133, 155, -10, -45]
+
+def get_rez_mask():
+    return xr.open_dataset('/g/data/w42/dr6273/work/projects/Aus_energy/data/rez_mask_era5_grid.nc').REZ
+
+def get_gccsa_mask():
+    return xr.open_dataset( '/g/data/w42/dr6273/work/projects/Aus_energy/data/gccsa_mask_era5_grid.nc').GCCSA
+
+def get_regions_from_region_codes(region_codes, mask):
+    regions = [mask.region.values[i] for i in range(len(mask.region.values)) if
+               mask.region_code.values[i] in region_codes]
+    return regions
+
+# ============================================================================
+# Data wrangling
+# ============================================================================
 
 def open_era_data(root_path,
                   variable,
@@ -83,6 +78,86 @@ def open_era_data(root_path,
     return xr.concat(ds_list, dim=concat_dim)
 
 # ============================================================================
+# Defining and computing events
+# ============================================================================
+
+def get_events(da, thresh, tail='lower'):
+    if tail == 'lower':
+        return xr.where(da < thresh, 1, 0)
+    elif tail == 'upper':
+        return xr.where(da > thresh, 1, 0)
+    else:
+        raise ValueError('Incorrect string for tail')
+        
+def calculate_event_frequency(da, thresh,
+                              tail, time_name='time'):
+    T = len(da[time_name].values)
+    events = get_events(da, thresh, tail)
+    freq = events.sum(time_name) / T
+            
+    return freq
+
+def calculate_compound_frequency(da1, da2, thresh1, thresh2, tail='lower', time_name='time'):
+    if tail == 'lower':
+        freq = xr.where(
+            (da1 < thresh1) &
+            (da2 < thresh2),
+            1, 0
+        ).sum(time_name) / len(da1[time_name].values)
+    else:
+        raise ValueError("Incorrect tail")
+        
+    return freq
+
+def get_event_years(da, thresh, tail='lower', time_name='time'):
+    """
+    Get the event years and other years of da.
+    """
+    if tail == 'lower':
+        event_years = da.where(
+            da < thresh
+        ).dropna(time_name)[time_name].dt.year.values
+
+        other_years = da.where(
+            da >= thresh
+        ).dropna(time_name)[time_name].dt.year.values
+    elif tail == 'upper':
+        event_years = da.where(
+            da > thresh
+        ).dropna(time_name)[time_name].dt.year.values
+
+        other_years = da.where(
+            da <= thresh
+        ).dropna(time_name)[time_name].dt.year.values
+    
+    return event_years, other_years
+
+def concurrent_lulls(da, region_codes, mask):
+    return da.sel(
+        region=get_regions_from_region_codes(region_codes, mask)
+    ).sum('region')
+
+def concurrent_univariate_or_compound_lulls(compound_da, univ_da, region_codes, mask, var_name='event'):
+    events = xr.where(
+        compound_da == 0,
+        univ_da, 0
+    ).sel(
+        region=get_regions_from_region_codes(region_codes, mask)
+    ).sum('region')
+    events = events.to_dataset(name=var_name)
+    return events[var_name]
+
+def get_all_events(da1, da2, count_compound_as_double=True, var_name='event'):
+    events = da1 + da2
+    
+    if count_compound_as_double == False: # Set compound days (coded as a 2) to 1.
+        events = xr.where(events == 2, 1, events)
+        
+    events = events.sum('region')
+    
+    return events.to_dataset(name=var_name)[var_name]
+
+# ============================================================================
 # Plotting
 # ============================================================================
 
@@ -116,3 +191,32 @@ def adjust_lightness(color, amount=0.5):
         c = color
     c = colorsys.rgb_to_hls(*matplotlib.colors.to_rgb(c))
     return colorsys.hls_to_rgb(c[0], max(0, min(1, amount * c[1])), c[2])
+
+# ============================================================================
+# Misc
+# ============================================================================
+
+def get_seasons():
+    return {
+        'Annual': range(1, 13),
+        'Winter': [6, 7, 8],
+        'Spring': [9, 10, 11],
+        'Summer': [12, 1, 2],
+        'Autumn': [3, 4, 5]
+    }
+
+def round_decimals_up(number:float, decimals:int=2):
+    """
+    Returns a value rounded up to a specific number of decimal places.
+    
+    From: https://kodify.net/python/math/round-decimals/
+    """
+    if not isinstance(decimals, int):
+        raise TypeError("decimal places must be an integer")
+    elif decimals < 0:
+        raise ValueError("decimal places has to be 0 or more")
+    elif decimals == 0:
+        return math.ceil(number)
+
+    factor = 10 ** decimals
+    return math.ceil(number * factor) / factor
